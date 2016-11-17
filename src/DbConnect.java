008b822d173cc9385b025294050e63f8206d7252
joy <joyc_y@hotmail.com>
2016-11-17 10:29:03 -0800

    added a dbconnect class

---
+++ src/DbConnect.java
@@ -0,0 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class DbConnect {
+	public void dbConn(){
+		final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+		final String DB_URL = "jdbc:mysql://ec2-54-226-9-216.compute-1.amazonaws.com/f2016_s1_user12";
+		final String USER = "f2016_s1_user12";
+		final String PASS = "f2016_s1_user12";
+		Connection conn = null;
+		Statement stmt = null;
+		try{
+			Class.forName("com.mysql.jdbc.driver");
+			System.out.println("Connecting to database...");
+			conn = DriverManager.getConnection(DB_URL,USER,PASS);
+			System.out.println("Creating statement...");
+			stmt = conn.createStatement();
+			String sql;
+			sql = "SELECT Designation, HourlyRate from Designation Rate ";
+			ResultSet rs = stmt.executeQuery(sql);
+			while(rs.next()){
+				String value = rs.getString("Designation");
+				System.out.println("ID: "+value);
+				rs.close();
+				stmt.close();
+				conn.close();
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println("Goodbye!");
+	}
+}
